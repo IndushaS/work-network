1bb5f86728f48ae30050d02199fe638097497971
Hamza Kazmi <hamza@Hamzas-MacBook-Pro.local>
2020-04-03 14:37:12 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Employees from './components/employees';
+import fire from '.src/config/fire'
+import Login from '.src/Login.js';
+import Home from '.src/Home.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Employees/>
+        { this.state.user ? ( <Home /> ) : ( <Login /> ) }
       </div>
     );
   }
---
+++ client/src/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import fire from './config/fire';
+
+class Home extends React.Component {
+
+
+
+  logout() {
+    fire.auth().signOut();
+  }
+
+
+
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>You Are Logged In</h1>
+
+        <button onClick = {this.logout}>Logout</button>
+
+
+   
+          <p>ID: {fire.auth().currentUser.uid}</p>
+
+   
+       
+
+
+      </div>
+    )
+  }
+
+
+}
+
+
+
+
+export default Home;
